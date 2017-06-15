2648fb7e70a23178cefdb38dfeb26753cbb11a32
taohs <50584211@qq.com>
2017-06-16 00:26:02 +0800

    add javaunit

---
+++ src/com/makotojava/intro/PersonTest.java
@@ -0,0 +1,14 @@
+package com.makotojava.intro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
